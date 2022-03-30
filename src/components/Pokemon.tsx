d0839fba1bd438b8bbbe25ef386f71c1443e4eab
Stivenprod <asdm1098@gmail.com>
2022-03-30 11:37:56 -0500

    map types

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -13,34 +13,38 @@ export const Pokemon = (props: any) => {
     updateFavoritePokemons(pokemon.name);
   };
 
+  const handlePokemon = () => {
+    console.log('click')!
+  }
+
   return (
     <div className="pokemon-card">
-      <div className="pokemon-img-container">
+      <div className="pokemon-img-container" onClick={ handlePokemon }>
         <img
           src={pokemon?.urlImg}
           alt={pokemon.name}
           className="pokemon-img"
         />
       </div>
-      <div className="card-body">
-        <div className="card-top">
+      <div className="card-body" >
+        <div className="card-top" onClick={ handlePokemon }>
           <h3>{pokemon.name}</h3>
           <div>#{pokemon.id}</div>
         </div>
         <div className="card-bottom">
           <div className="pokemon-type">
-            {
+            {/* {
               <div className="pokemon-type-text">
                 {pokemon.type}
               </div>
-            }
-            {/* {pokemon.types.map((type: any, idx: number) => {
+            } */}
+            {pokemon.types.map((type: any, idx: number) => {
               return (
                 <div key={idx} className="pokemon-type-text">
-                  {type.type.name}
+                  {type}
                 </div>
               );
-            })} */}
+            })}
           </div>
           <button onClick={clickHeart} className="pokemon-heart-btn">
             <div className="pokemon-favorite">{heart}</div>
